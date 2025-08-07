6ef505cc534076a18ea3d983451d23a3eb00d589
Frederic-K <frederic.kreuder.pro@gmail.com>
2025-08-07 20:45:41 +0200

    add generation of api key

---
+++ src/utils/generateApiKey.js
@@ -0,0 +1,8 @@
+import crypto from 'crypto';
+
+function generateApiKey(length = 32) {
+  return crypto.randomBytes(length).toString('hex');
+}
+
+const apiKey = generateApiKey();
+console.log(`Generated API Key: ${apiKey}`);
